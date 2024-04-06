f6cfa41fe43eef990e31665bb3b5958f7f77be69
Jesthom-Go <160304392+Jesthom-Go@users.noreply.github.com>
2024-04-06 22:37:34 +0800

    3 key door finally finished
    the door will need 3 keys to 'open'

--- Escape the Finals/Assets/Scripts/KeyChecker.cs
+++ Escape the Finals/Assets/Scripts/KeyChecker.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KeyChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject intText, key1, key2, key3, lockedText;
+    public bool interactable, toggle;
+    //SHows e
+    void OnTriggerStay(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            intText.SetActive(true);
+            interactable = true;
+        }
     }
 
-    // Update is called once per frame
+    //E gone
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            intText.SetActive(false);
+            interactable = false;
+        }
+    }
     void Update()
     {
-        
+        //interacts with door
+        if (interactable == true)
+        {
+            //Press E on door
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //When the player has the key
+                if (key1.active == false && key2.active == false && key3.active == false)
+                {
+                    //switches to next scene
+                    toggle = !toggle;
+                    if (toggle == true)
+                    {
+                          Debug.Log("banana");
+/*                        loadingScreen.SetActive(true);*/
+                    //put the end scene here v
+/*                        SceneManager.LoadScene("Level2");*/
+                    }
+                    intText.SetActive(false);
+                    interactable = false;
+                }
+                //When the player doesnt ahve the key
+                if (key1.active == true && key2.active == true && key3.active == true)
+                {
+                    lockedText.SetActive(true); 
+                    StopCoroutine("disableText");
+                    StartCoroutine("disableText");
+                }
+            }
+        }
     }
+IEnumerator disableText()
+    {
+        yield return new WaitForSeconds(2.0f);
+        lockedText.SetActive(false);
+    }
+
 }
